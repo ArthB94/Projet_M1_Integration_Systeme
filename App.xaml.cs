c389aada65f9237e688631bd72905e40318fa8f4
ArthB94 <80051198+ArthB94@users.noreply.github.com>
2023-09-21 16:58:39 +0200

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Projet_M1_Integration_Systeme
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projet_M1_Integration_Systeme
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LbxUtilisateurs.ItemsSource = new List<Utilisateur>
+            {
+                new Utilisateur { Nom = "Jean" },
+                new Utilisateur { Nom = "Paul" },
+                new Utilisateur { Nom = "Jacques" }
+            };
+        }
+    }
+}
